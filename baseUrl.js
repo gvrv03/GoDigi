9fc94a6c4354a0dedbcac37884ff1c1a31383ac2
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-04-28 11:51:29 +0530

    try to fix 4

--- baseUrl.js
+++ baseUrl.js
@@ -3,7 +3,7 @@ let baseUrl = "";
 if (process.env.NODE_ENV == "development") {
   baseUrl = "http://localhost:3000/";
 } else {
-  baseUrl = "https://www.gauravnarnaware.com/";
+  baseUrl = "https://godiginew.vercel.app/";
 }
 
 export default baseUrl;
--- src/app/page.js
+++ src/app/page.js
@@ -4,69 +4,6 @@ import FeatureBlog from "@/Components/Home/FeatureBlog";
 import LandingPage from "@/Components/Home/LandingPage";
 import Skills from "@/Components/Home/Skills";
 
-export const metadata = {
-  title: "GoDigi || Personal Website ",
-  applicationName: "GoDigi || Personal Website ",
-  description:
-    "  Hi, I'm GoDigi, an Indian web developer   working out of Maharashtra. I have a strong desire to make the technical and aesthetic components of digital goods come to life. When I'm creating, learning, exploring, and considering ways to improve things, I'm happy.",
-  keywords: [
-    "its.gvrv_n",
-    "gvrv_n",
-    "GoDigi",
-    "GoDigi Ghatanji",
-    "Ghatanji",
-    "Narnaware Garuav",
-  ],
-  images: ["/gaurav.svg"],
-  url: "/",
-  twitter: {
-    card: "summary_large_image",
-    title: "GoDigi || Personal Website ",
-    url: "/",
-    images: ["/gaurav.svg"],
-    creator: "@gvrv_n",
-    description:
-      "  Hi, I'm GoDigi, an Indian web developer working out of Maharashtra. I have a strong desire to make the technical and aesthetic components of digital goods come to life. When I'm creating, learning, exploring, and considering ways to improve things, I'm happy.",
-    keywords: [
-      "its.gvrv_n",
-      "gvrv_n",
-      "GoDigi",
-      "GoDigi Ghatanji",
-      "Ghatanji",
-      "Narnaware Garuav",
-    ],
-  },
-  openGraph: {
-    title: "GoDigi || Personal Website ",
-    url: "/",
-    images: ["/gaurav.svg"],
-    description:
-      "  Hi, I'm GoDigi, an Indian web developer working out of Maharashtra. I have a strong desire to make the technical and aesthetic components of digital goods come to life. When I'm creating, learning, exploring, and considering ways to improve things, I'm happy.",
-    keywords: [
-      "its.gvrv_n",
-      "gvrv_n",
-      "GoDigi",
-      "GoDigi Ghatanji",
-      "Ghatanji",
-      "Narnaware Garuav",
-    ],
-  },
-
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-};
-
 export default function Home() {
   return (
     <main>
